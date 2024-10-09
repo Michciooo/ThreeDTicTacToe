ad596ea798fa555185637ee895c2e3032320424a
IB2R5I <danielgarbuszewski2@gmail.com>
2024-10-09 21:15:20 +0200

    poprawiłem ci tego popUp'a

--- GamePlay.cs
+++ GamePlay.cs
@@ -10,27 +10,27 @@ public partial class GamePlay : Control
 {
 	bool _playerTurn = true;
 	public List<Button> TttBtns = new List<Button>();
-	public List<Button> FirstDBtn = new List<Button>(); 
-	public List<Button> SecondDBtn = new List<Button>(); 
+	public List<Button> FirstDBtn = new List<Button>();
+	public List<Button> SecondDBtn = new List<Button>();
 	public List<Button> ThirdDBtn = new List<Button>();
-	
+
 	public List<Label3D> Labels = new List<Label3D>();
 	public override void _Ready()
 	{
 		Button restartButton = GetNode<Button>("restartBtn");
-		
+
 		VBoxContainer lay1 = GetNode<VBoxContainer>("firstD/lay1");
 		VBoxContainer lay2 = GetNode<VBoxContainer>("firstD/lay2");
 		VBoxContainer lay3 = GetNode<VBoxContainer>("firstD/lay3");
-		
+
 		VBoxContainer lay4 = GetNode<VBoxContainer>("secondD/lay4");
 		VBoxContainer lay5 = GetNode<VBoxContainer>("secondD/lay5");
 		VBoxContainer lay6 = GetNode<VBoxContainer>("secondD/lay6");
-		
+
 		VBoxContainer lay7 = GetNode<VBoxContainer>("thirdD/lay7");
 		VBoxContainer lay8 = GetNode<VBoxContainer>("thirdD/lay8");
 		VBoxContainer lay9 = GetNode<VBoxContainer>("thirdD/lay9");
-		
+
 		Create_Dimensions(lay1, lay2, lay3, lay4, lay5, lay6, lay7, lay8, lay9);
 		restartButton.Pressed += RestartGame;
 	}
@@ -50,8 +50,8 @@ public partial class GamePlay : Control
 	}
 
 	public void Create_Dimensions(VBoxContainer lay1, VBoxContainer lay2, VBoxContainer lay3,
-		VBoxContainer lay4 , VBoxContainer lay5, VBoxContainer lay6,
-		VBoxContainer lay7 , VBoxContainer lay8, VBoxContainer lay9)
+		VBoxContainer lay4, VBoxContainer lay5, VBoxContainer lay6,
+		VBoxContainer lay7, VBoxContainer lay8, VBoxContainer lay9)
 	{
 		StyleBoxFlat bgColor = new StyleBoxFlat();
 		for (int x = 0; x < 3; x++)
@@ -63,28 +63,28 @@ public partial class GamePlay : Control
 					Button btn = new Button();
 					btn.SetDefaultCursorShape(CursorShape.PointingHand);
 					btn.CustomMinimumSize = new Vector2(100, 100);
-					
-					if (y==0 && z == 0) lay1.AddChild(btn);
-					if (y==0 && z == 1) lay2.AddChild(btn);
-					if (y==0 && z == 2) lay3.AddChild(btn);
-					
-					if (y==1 && z == 0) lay4.AddChild(btn);
-					if (y==1 && z == 1) lay5.AddChild(btn);
-					if (y==1 && z == 2) lay6.AddChild(btn);
-					
-					if (y==2 && z == 0) lay7.AddChild(btn);
-					if (y==2 && z == 1) lay8.AddChild(btn);
-					if (y==2 && z == 2) lay9.AddChild(btn);
-				
-					if(y==0) FirstDBtn.Add(btn);
-					if(y==1) SecondDBtn.Add(btn);
-					if(y==2) ThirdDBtn.Add(btn);
-					
+
+					if (y == 0 && z == 0) lay1.AddChild(btn);
+					if (y == 0 && z == 1) lay2.AddChild(btn);
+					if (y == 0 && z == 2) lay3.AddChild(btn);
+
+					if (y == 1 && z == 0) lay4.AddChild(btn);
+					if (y == 1 && z == 1) lay5.AddChild(btn);
+					if (y == 1 && z == 2) lay6.AddChild(btn);
+
+					if (y == 2 && z == 0) lay7.AddChild(btn);
+					if (y == 2 && z == 1) lay8.AddChild(btn);
+					if (y == 2 && z == 2) lay9.AddChild(btn);
+
+					if (y == 0) FirstDBtn.Add(btn);
+					if (y == 1) SecondDBtn.Add(btn);
+					if (y == 2) ThirdDBtn.Add(btn);
+
 					bgColor.BgColor = new Color("#000000");
-				
-					btn.AddThemeStyleboxOverride("normal",bgColor);
+
+					btn.AddThemeStyleboxOverride("normal", bgColor);
 					TttBtns.Add(btn);
-					
+
 					btn.Pressed += () => Logic(btn);
 				}
 			}
@@ -106,10 +106,10 @@ public partial class GamePlay : Control
 				}
 			}
 		}
-		
+
 		Label playerTurnLabel = GetNode<Label>("playerTurnLabel");
 		Label3D label3D = main.BtnAndboxMesLabel3DDictionary[btn];
-		
+
 		if (btn.Text != "")
 		{
 			return;
@@ -133,10 +133,7 @@ public partial class GamePlay : Control
 
 	public void WhoWon()
 	{
-		WinPopUp winPopUp= GetNode<WinPopUp>("/root/Main/WinPopUp");
-		Label winLabel = GetNode<Label>("/root/Main/WinPopUp/winLabel");
-		Button closeBtn = GetNode<Button>("/root/Main/WinPopUp/closeBtn");
-		
+
 		Button[,] wins =
 		{
 			// grid 1 H
@@ -146,7 +143,7 @@ public partial class GamePlay : Control
 			{ FirstDBtn[0], FirstDBtn[3], FirstDBtn[6] },
 			{ FirstDBtn[1], FirstDBtn[4], FirstDBtn[7] },
 			{ FirstDBtn[2], FirstDBtn[5], FirstDBtn[8] },
-			{ FirstDBtn[0], FirstDBtn[4], FirstDBtn[8] },  
+			{ FirstDBtn[0], FirstDBtn[4], FirstDBtn[8] },
 			{ FirstDBtn[2], FirstDBtn[4], FirstDBtn[6] },
             
 			//grid 2 H
@@ -156,7 +153,7 @@ public partial class GamePlay : Control
 			{ SecondDBtn[0], SecondDBtn[3], SecondDBtn[6] },
 			{ SecondDBtn[1], SecondDBtn[4], SecondDBtn[7] },
 			{ SecondDBtn[2], SecondDBtn[5], SecondDBtn[8] },
-			{ SecondDBtn[0], SecondDBtn[4], SecondDBtn[8] },  
+			{ SecondDBtn[0], SecondDBtn[4], SecondDBtn[8] },
 			{ SecondDBtn[2], SecondDBtn[4], SecondDBtn[6] },
             
 			//grid 3 H
@@ -166,7 +163,7 @@ public partial class GamePlay : Control
 			{ ThirdDBtn[0], ThirdDBtn[3], ThirdDBtn[6] },
 			{ ThirdDBtn[1], ThirdDBtn[4], ThirdDBtn[7] },
 			{ ThirdDBtn[2], ThirdDBtn[5], ThirdDBtn[8] },
-			{ ThirdDBtn[0], ThirdDBtn[4], ThirdDBtn[8] },  
+			{ ThirdDBtn[0], ThirdDBtn[4], ThirdDBtn[8] },
 			{ ThirdDBtn[2], ThirdDBtn[4], ThirdDBtn[6] },
             
 			//grid 1 V
@@ -179,15 +176,15 @@ public partial class GamePlay : Control
 			{ FirstDBtn[6] , SecondDBtn[6] , ThirdDBtn[6]},
 			{ FirstDBtn[7] , SecondDBtn[7] , ThirdDBtn[7]},
 			{ FirstDBtn[8] , SecondDBtn[8] , ThirdDBtn[8]},
-            
+
 			{ FirstDBtn[6] , SecondDBtn[3] , ThirdDBtn[0]},
 			{ FirstDBtn[7] , SecondDBtn[4] , ThirdDBtn[1]},
 			{ FirstDBtn[8] , SecondDBtn[5] , ThirdDBtn[2]},
-            
+
 			{ FirstDBtn[0] , SecondDBtn[3] , ThirdDBtn[6]},
 			{ FirstDBtn[1] , SecondDBtn[4] , ThirdDBtn[7]},
 			{ FirstDBtn[2] , SecondDBtn[5] , ThirdDBtn[8]},
-            
+
 			{ FirstDBtn[0], SecondDBtn[4], ThirdDBtn[8]},
 			{ FirstDBtn[2], SecondDBtn[4], ThirdDBtn[6]},
 			{ FirstDBtn[6] , SecondDBtn[4], ThirdDBtn[2]},
@@ -196,12 +193,16 @@ public partial class GamePlay : Control
 		for (int i = 0; i < wins.GetLength(0); i++)
 		{
 			if (wins[i, 0].Text == wins[i, 1].Text && wins[i, 1].Text == wins[i, 2].Text &&
-			    (wins[i, 0].Text == "X" || wins[i, 0].Text == "O"))
+				(wins[i, 0].Text == "X" || wins[i, 0].Text == "O"))
 			{
-				winPopUp.Visible = true;
-				winLabel.Visible = true;
-				closeBtn.Visible = true;
-				winLabel.Text = $"Wygrał : {wins[i, 0].Text}";
+				// tutaj pobieram scenę wczytując ją
+				var popUp = GD.Load<PackedScene>("res://WinPopUp.tscn");
+				// inicjalizacja 
+				var popUpInsant = popUp.Instantiate();
+				// dodaje tekst ten co miałeś tylko że po zrobieniu instancji przed dodaniem na ekran
+				popUpInsant.GetNode<Label>("winLabel").Text = $"Wygrał : {wins[i, 0].Text}";
+				// dodanie na ekran
+				GetTree().Root.AddChild(popUpInsant);
 				return;
 			}
 		}
--- WinPopUp.cs
+++ WinPopUp.cs
@@ -5,23 +5,18 @@ namespace threeDTicTacToe;
 
 public partial class WinPopUp : Control
 {
-    public override void _Ready()
-    { 
-        Button closeBtn = GetNode<Button>("closeBtn");
-        closeBtn.Pressed += ClosePopUp;
-    }
-    private void ClosePopUp()
-    {
-        Button closeBtn = GetNode<Button>("closeBtn");
-        Label winLabel = GetNode<Label>("winLabel");
-        
-        this.Visible = false;
-        closeBtn.Visible = false;
-        winLabel.Visible = false;
-    }
+	public override void _Ready()
+	{
+		Button closeBtn = GetNode<Button>("closeBtn");
+		closeBtn.Pressed += ClosePopUp;
+	}
+	private void ClosePopUp()
+	{
+		QueueFree(); // usuwa ci instancje popatrz podczas na te zdalny podgląd jak się tworzy
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
 }

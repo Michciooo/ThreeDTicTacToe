ce453b4047ed2dc63808d6c964c60e30a2506055
Michal <rfrttfgtrtfgrr>
2024-10-23 18:04:00 +0200

    binding re-work

--- Global.cs
+++ Global.cs
@@ -4,9 +4,9 @@ using System;
 namespace threeDTicTacToe;
 public partial class Global : Node
 {
-    public Key mainMenuKey = Key.Escape;
-    public Key appExitKey = Key.Q;
-    public Key restartPosCubeKey = Key.R;
-    public Key shiftLockKey = Key.Shift;
-    public Key unShiftLockKey = Key.Ctrl;
+    public string mainMenuKey = "Escape";
+    public string appExitKey = "Q";
+    public string restartPosCubeKey = "R";
+    public string shiftLockKey = "Shift";
+    public string unShiftLockKey = "Ctrl";
 }
--- Settings.cs
+++ Settings.cs
@@ -5,71 +5,169 @@ namespace threeDTicTacToe
 {
     public partial class Settings : Control
     {
-        // public Key[] keysList = new[]
-        // {
-        //     Key.Key0, Key.Key1, Key.Key2, Key.Key3, Key.Key4, Key.Key5, Key.Key6, Key.Key7, Key.Key8, Key.Key9,
-        //     Key.Minus, Key.Equal, Key.Backslash, Key.Backspace, Key.Escape,
-        //     Key.F1, Key.F2, Key.F3, Key.F4, Key.F5, Key.F6, Key.F7, Key.F8, Key.F9, Key.F10, Key.F11, Key.F12,
-        //     Key.Tab, Key.Q, Key.W, Key.E, Key.R, Key.T, Key.Y, Key.U, Key.I, Key.O, Key.P,
-        //     Key.Bracketleft, Key.Bracketright, Key.Enter, Key.Capslock, Key.A, Key.S, Key.D, Key.F, Key.G, 
-        //     Key.H, Key.J, Key.K, Key.L, Key.Semicolon, Key.Apostrophe, Key.Shift, 
-        //     Key.Z, Key.X, Key.C, Key.V, Key.B, Key.N, Key.M,
-        //     Key.Comma, Key.Period, Key.Slash, Key.Ctrl, Key.Alt, Key.Space,
-        //     Key.Up, Key.Down, Key.Left, Key.Right, 
-        //     Key.Delete, Key.Home, Key.End, Key.Pageup, Key.Pagedown,
-        //     Key.Print, Key.Scrolllock, Key.Pause, 
-        //     Key.KpDivide, Key.KpMultiply, Key.KpSubtract, 
-        //     Key.KpAdd, Key.KpEnter, Key.KpPeriod, 
-        //     Key.Kp0, Key.Kp1, Key.Kp2, Key.Kp3, Key.Kp4, Key.Kp5, Key.Kp6, Key.Kp7, Key.Kp8, Key.Kp9
-        // };
-
         public override void _Ready()
         {
+            var shiftLockTextInput = this.GetNode<TextEdit>("Main/KeysInput/shiftLock");
+            var unshiftLockTextInput = this.GetNode<TextEdit>("Main/KeysInput/unShiftLock");
+            var restartPosCubeTextInput = this.GetNode<TextEdit>("Main/KeysInput/restartPosCube");
+            var appExitTextInput = this.GetNode<TextEdit>("Main/KeysInput/appExit");
+            var mainMenuTextInput = this.GetNode<TextEdit>("Main/KeysInput/mainMenu");
+            
+            var btn1 = this.GetNode<Button>("Main/RestartBtns/btn1");
+            var btn2 = this.GetNode<Button>("Main/RestartBtns/btn2");
+            var btn3 = this.GetNode<Button>("Main/RestartBtns/btn3");
+            var btn4 = this.GetNode<Button>("Main/RestartBtns/btn4");
+            var btn5 = this.GetNode<Button>("Main/RestartBtns/btn5");
+            
             Binding();
+
+            btn1.Pressed += btnPress;
+            btn2.Pressed += btnPress;
+            btn3.Pressed += btnPress;
+            btn4.Pressed += btnPress;
+            btn5.Pressed += btnPress;
         }
 
         private void Binding()
         {
             Global global = (Global)GetNode("/root/Global");
-    
-            var shiftLockTextInput = GetNodeOrNull<TextEdit>("/root/Settings/Main/KeysInput/shiftLock");
-            if (shiftLockTextInput == null) 
-            {
-                GD.PrintErr("Node not found: shiftLock");
-            }
-            var unshiftLockTextInput = GetNodeOrNull<TextEdit>("/root/Settings/Main/KeysInput/unShiftLock");
-            if (unshiftLockTextInput == null) 
-            {
-                GD.PrintErr("Node not found: unShiftLock");
-            }
-            var restartPosCubeTextInput = GetNodeOrNull<TextEdit>("/root/Settings/Main/KeysInput/restartPosCube");
-            if (restartPosCubeTextInput == null) 
-            {
-                GD.PrintErr("Node not found: restartPosCube");
-            }
-            var mainMenuTextInput = GetNodeOrNull<TextEdit>("/root/Settings/Main/KeysInput/mainMenu");
-            if (mainMenuTextInput == null) 
-            {
-                GD.PrintErr("Node not found: mainMenu");
-            }
-            var appExitTextInput = GetNodeOrNull<TextEdit>("/root/Settings/Main/KeysInput/appExit");
-            if (appExitTextInput == null) 
-            {
-                GD.PrintErr("Node not found: appExit");
-            }
+            var shiftLockTextInput = this.GetNode<TextEdit>("Main/KeysInput/shiftLock");
+            var unshiftLockTextInput = this.GetNode<TextEdit>("Main/KeysInput/unShiftLock");
+            var restartPosCubeTextInput = this.GetNode<TextEdit>("Main/KeysInput/restartPosCube");
+            var appExitTextInput = this.GetNode<TextEdit>("Main/KeysInput/appExit");
+            var mainMenuTextInput = this.GetNode<TextEdit>("Main/KeysInput/mainMenu");
+            
 
-            // Assign Text values only if nodes are not null
-            if (shiftLockTextInput != null)
-                shiftLockTextInput.Text = global.shiftLockKey.ToString();
-            if (unshiftLockTextInput != null)
-                unshiftLockTextInput.Text = global.unShiftLockKey.ToString();
-            if (restartPosCubeTextInput != null)
-                restartPosCubeTextInput.Text = global.restartPosCubeKey.ToString();
-            if (appExitTextInput != null)
-                appExitTextInput.Text = global.appExitKey.ToString();
-            if (mainMenuTextInput != null)
-                mainMenuTextInput.Text = global.mainMenuKey.ToString();
+            shiftLockTextInput.Text = global.shiftLockKey.ToString();
+            unshiftLockTextInput.Text = global.unShiftLockKey.ToString();
+            restartPosCubeTextInput.Text = global.restartPosCubeKey.ToString();
+            appExitTextInput.Text = global.appExitKey.ToString();
+            mainMenuTextInput.Text = global.mainMenuKey.ToString();
+            
         }
+
+        private void btnPress()
+        {
+            Global global = (Global)GetNode("/root/Global");
+            var btn1 = this.GetNode<Button>("Main/RestartBtns/btn1");
+            var btn2 = this.GetNode<Button>("Main/RestartBtns/btn2");
+            var btn3 = this.GetNode<Button>("Main/RestartBtns/btn3");
+            var btn4 = this.GetNode<Button>("Main/RestartBtns/btn4");
+            var btn5 = this.GetNode<Button>("Main/RestartBtns/btn5");
+            
+            var shiftLockTextInput = this.GetNode<TextEdit>("Main/KeysInput/shiftLock");
+            var unshiftLockTextInput = this.GetNode<TextEdit>("Main/KeysInput/unShiftLock");
+            var restartPosCubeTextInput = this.GetNode<TextEdit>("Main/KeysInput/restartPosCube");
+            var appExitTextInput = this.GetNode<TextEdit>("Main/KeysInput/appExit");
+            var mainMenuTextInput = this.GetNode<TextEdit>("Main/KeysInput/mainMenu");
+            if (btn1.IsPressed())
+            {
+                shiftLockTextInput.Text = "Shift";
+                global.shiftLockKey = shiftLockTextInput.Text;
+                AddKeyToAction("shiftLock" , Key.Shift);
+            }
+            if (btn2.IsPressed())
+            {
+                unshiftLockTextInput.Text = "Ctrl";
+                global.unShiftLockKey = unshiftLockTextInput.Text;
+                AddKeyToAction("unshiftLock" , Key.Ctrl);
+            }
+            if (btn3.IsPressed())
+            {
+                restartPosCubeTextInput.Text = "R";
+                global.restartPosCubeKey = restartPosCubeTextInput.Text;
+                AddKeyToAction("resetPosCube" , Key.R);
+            }
+            if (btn4.IsPressed())
+            {
+                mainMenuTextInput.Text = "Escape";
+                global.mainMenuKey = mainMenuTextInput.Text;
+                AddKeyToAction("mainMenu" , Key.Escape);
+            }
+            if (btn5.IsPressed())
+            {
+                appExitTextInput.Text = "Q";
+                global.appExitKey = appExitTextInput.Text;
+                AddKeyToAction("appExit" , Key.Q);
+            }
+        }
+
+
+        public override void _Input(InputEvent @event)
+        {
+            Global global = (Global)GetNode("/root/Global");
+            var shiftLockTextInput = this.GetNode<TextEdit>("Main/KeysInput/shiftLock");
+            var unshiftLockTextInput = this.GetNode<TextEdit>("Main/KeysInput/unShiftLock");
+            var restartPosCubeTextInput = this.GetNode<TextEdit>("Main/KeysInput/restartPosCube");
+            var appExitTextInput = this.GetNode<TextEdit>("Main/KeysInput/appExit");
+            var mainMenuTextInput = this.GetNode<TextEdit>("Main/KeysInput/mainMenu");
+            if (@event is InputEventKey eventKey && eventKey.Pressed)
+            {
+                if (shiftLockTextInput.HasFocus())
+                {
+                    if (unshiftLockTextInput.Text == shiftLockTextInput.Text ||
+                        appExitTextInput.Text == shiftLockTextInput.Text ||
+                        restartPosCubeTextInput.Text == shiftLockTextInput.Text
+                        || mainMenuTextInput.Text == shiftLockTextInput.Text)
+                    {
+                        shiftLockTextInput.Text = shiftLockTextInput.Text;
+                    }
+                    else
+                    {
+                        shiftLockTextInput.Text = OS.GetKeycodeString(eventKey.Keycode);
+                        global.shiftLockKey = shiftLockTextInput.Text;
+                        AddKeyToAction("shiftLock" , eventKey.Keycode);
+                    }
+                }
+                if (unshiftLockTextInput.HasFocus())
+                {
+                    unshiftLockTextInput.Text = OS.GetKeycodeString(eventKey.Keycode);
+                    global.unShiftLockKey = unshiftLockTextInput.Text;
+                    AddKeyToAction("unshiftLock" , eventKey.Keycode);
+                }
+                if (restartPosCubeTextInput.HasFocus())
+                {
+                    restartPosCubeTextInput.Text = OS.GetKeycodeString(eventKey.Keycode);
+                    global.restartPosCubeKey = restartPosCubeTextInput.Text;
+                    AddKeyToAction("resetPosCube" , eventKey.Keycode);
+                }
+                if (appExitTextInput.HasFocus())
+                {
+                    appExitTextInput.Text = OS.GetKeycodeString(eventKey.Keycode);
+                    global.appExitKey = appExitTextInput.Text;
+                    AddKeyToAction("appExit" , eventKey.Keycode);
+                }
+                if (mainMenuTextInput.HasFocus())
+                {
+                    if (unshiftLockTextInput.Text == mainMenuTextInput.Text ||
+                        appExitTextInput.Text == mainMenuTextInput.Text ||
+                        restartPosCubeTextInput.Text == mainMenuTextInput.Text
+                        || shiftLockTextInput.Text == mainMenuTextInput.Text)
+                    {
+                        mainMenuTextInput.Text = mainMenuTextInput.Text;
+                    }
+                    else
+                    {
+                        mainMenuTextInput.Text = OS.GetKeycodeString(eventKey.Keycode);
+                        global.mainMenuKey = mainMenuTextInput.Text;
+                        AddKeyToAction("mainMenu" , eventKey.Keycode);
+                    }
+                }
+            }
+            
+        }
+
+        private void AddKeyToAction(string actionName, Key keycode)
+        {
+            if (InputMap.HasAction(actionName))
+            {
+                InputMap.ActionEraseEvents(actionName);
+                InputEventKey inputEventKey = new InputEventKey();
+                inputEventKey.Keycode = keycode;
+                InputMap.ActionAddEvent(actionName, inputEventKey);
+            }
+        }
+
         public override void _Process(double delta)
         {
             if (Input.IsActionPressed("mainMenu"))
